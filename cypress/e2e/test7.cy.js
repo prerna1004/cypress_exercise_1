862012d3082e22aaa05c99cb82480e072a19a4f7
Prerna <prernaagarwal107@gmail.com>
2024-02-22 05:14:20 +0530

    mouse hover

---
+++ cypress/e2e/test7.cy.js
@@ -0,0 +1,23 @@
+
+describe('handling web tables',function()
+{
+it('it my first ',function()
+{
+    cy.visit("https://rahulshettyacademy.com/AutomationPractice/")
+
+    cy.get('.mouse-hover-content').invoke('show')
+    cy.contains('Top').click()
+    cy.url().should('include', 'top')
+
+    
+    
+    
+
+
+
+    
+    
+    
+})
+
+})
